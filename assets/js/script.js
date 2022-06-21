acae2f6770aa4869a7025345264f3f1c0b2daaab
rschragger <80379457+rschragger@users.noreply.github.com>
2022-06-21 13:17:37 +1000

    Templated files and directory structure

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//Create variables ---------------------------------------------------------
+//var IDs
+//defaults
+var defaultVals = {
+}
+
+//Other Variables required
+var varOne = [];
+
+
+// Functions ---------------------------------------------------------
+function init() {
+
+}
+
+init()
