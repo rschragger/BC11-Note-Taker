75dc56c5cac3ccbfab928194ca7260aeb1b2ce60
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-04 17:43:28 +1000

    Updates

--- routes/notes.js
+++ routes/notes.js
@@ -4,35 +4,44 @@ const fs = require('fs')
 notes.get('/', (req, res) => {
 
     // get notes from note file
-  
+
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) { console.log(err) }
-        else { res.send( JSON.parse(data) )}
+        else { res.send(JSON.parse(data)) }
     })
 
-   
-/*
-      res.send(`[
-        {
-            "title":"Test Title",
-            "text":"Test text"
-        },
-        {
-            "title":"Test Title2",
-            "text":"Test text2"
-        }
-    ]`)
-*/
-    console.log(`Get Notes ${res}`)
+
+    /*  res.send(`[{ "title":"Test Title","text":"Test text" },
+            { "title":"Test Title2","text":"Test text2"}]`) */
 
 });
+
+//I think this is handled INSIDE the html/js for the file and doesn't need api call
+/*
+notes.get('/:notes_id', (req, res) => {
+let noteId = req.params.notes_id ;
+
+    // get a certain note from note file
+
+   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) { console.log(err) }
+        else { (JSON.parse(data)) }
+    }).then((jsonData)=>{
+
+    })
+
+
+    //  res.send(`[{ "title":"Test Title","text":"Test text" }, { "title":"Test Title2","text":"Test text2"}]`) 
+
+});
+*/
 
 notes.post('/', (req, res) => {
 
     const notesBody = req.body;
     console.log('Save Note')
 
-   // push note into note file
+    // push note into note file
 
 });
 
@@ -40,7 +49,7 @@ notes.delete('/:notes_id', (req, res) => {
     noteID = req.params.notes_id;
 
     //use noteID to splice out object from array in Notes file 
-console.log('DeleteNote')
+    console.log('DeleteNote')
 });
 
 module.exports = notes;
